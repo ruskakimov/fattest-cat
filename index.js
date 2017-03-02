42dd9a66265b02cb03882817ba46fbe41a4a9b0d
Rustem Kakimov <ruskakimov@gmail.com>
2017-03-02 17:38:42 +0800

    Cats -> Dogs

--- index.js
+++ index.js
@@ -7,63 +7,63 @@ const cheerio         = require("cheerio");
 const {uniq, compact} = require("lodash");
 
 const SFSPCA_BASE = "https://www.sfspca.org"
-const ADOPTION_PAGE = `${SFSPCA_BASE}/adoptions/cats`;
+const ADOPTION_PAGE = `${SFSPCA_BASE}/adoptions/dogs`;
 
-const fetchCatsHelper = Promise.method((pageNumber, catsSoFar) => {
+const fetchDogsHelper = Promise.method((pageNumber, dogsSoFar) => {
   const url = pageNumber === 0 ? ADOPTION_PAGE : `${ADOPTION_PAGE}?page=${pageNumber}`
   return request.get(url)
     .then((adoptionsPage) => {
-      const cats = cheerio(adoptionsPage)
+      const dogs = cheerio(adoptionsPage)
         .find("a")
         .filter((i, tag) => tag.attribs.href && tag.attribs.href.match(/adoptions\/pet-details\/\d+/))
         .map((i, tag) => `${SFSPCA_BASE}${tag.attribs.href}`)
         .toArray();
-      if (!cats || cats.length === 0) {
-        return catsSoFar;
+      if (!dogs || dogs.length === 0) {
+        return dogsSoFar;
       } else {
-        return fetchCatsHelper(pageNumber + 1, catsSoFar.concat(cats));
+        return fetchDogsHelper(pageNumber + 1, dogsSoFar.concat(dogs));
       }
     })
     .catch((err) => {
-      console.log("Error fetching cats:", err);
-      return catsSoFar;
+      console.log("Error fetching dogs:", err);
+      return dogsSoFar;
     });
 });
-const fetchCats = () => fetchCatsHelper(0, []);
+const fetchDogs = () => fetchDogsHelper(0, []);
 
-console.log("Accessing San Francisco SPCA (Cat Department)...");
+console.log("Accessing San Francisco SPCA (dog Department)...");
 
-fetchCats()
-  .then(uniq) // NO DOUBLE CATS
-  .tap((cats) => console.log(`Cat information system accessed. ${cats.length} cats found. Beginning weighing process...`))
+fetchDogs()
+  .then(uniq) // NO DOUBLE DOGS
+  .tap((dogs) => console.log(`Dog information system accessed. ${dogs.length} dogs found. Beginning weighing process...`))
   .map((url) => {
     return request.get(url)
-      // SPCA sometimes returns 403s for some cats, ignore this.
+      // SPCA sometimes returns 403s for some dogs, ignore this.
       .catch((err) => err)
-      .then((catPage) => {
-        const $ = cheerio.load(catPage);
+      .then((dogPage) => {
+        const $ = cheerio.load(dogPage);
         const name = $(".field-name-title h1").text();
         const weight = $(".field-name-field-animal-weight .field-item").text();
         const lbs = Number(/(\d+)lbs\./.exec(weight)[1]);
         const oz = /(\d+)oz\./.test(weight) ? Number(/(\d+)oz\./.exec(weight)[1]) : 0;
         const isFemale = $(".field-name-field-gender .field-item").text().trim() === "Female";
 
-        console.log("Weighing cat:", name);
+        console.log("Weighing dog:", name);
         return {name, lbs, oz, isFemale, url}
       })
-      // Null for cats that cannot be parsed.
+      // Null for dogs that cannot be parsed.
       .catch(() => {});
   })
-  // Filter out unparsable cats.
+  // Filter out unparsable dogs.
   .then(compact)
-  .then((cats) => {
-    let fattestCat = {lbs: 0, oz: 0};
-    cats.forEach((cat) => {
-      if (cat.lbs > fattestCat.lbs || (cat.lbs === fattestCat.lbs && cat.oz > fattestCat.oz)) {
-        fattestCat = cat;
+  .then((dogs) => {
+    let fattestDog = {lbs: 0, oz: 0};
+    dogs.forEach((dog) => {
+      if (dog.lbs > fattestDog.lbs || (dog.lbs === fattestDog.lbs && dog.oz > fattestDog.oz)) {
+        fattestDog = dog;
       }
     });
-    console.log(`The fattest cat is ${fattestCat.name}. ${(fattestCat.isFemale ? "She" : "He")} weighs ${fattestCat.lbs} lbs and ${fattestCat.oz} oz.`);
-    setTimeout(() => console.log("Opening cat profile..."), 2000);
-    setTimeout(() => opener(fattestCat.url), 4000);
+    console.log(`The fattest dog is ${fattestDog.name}. ${(fattestDog.isFemale ? "She" : "He")} weighs ${fattestDog.lbs} lbs and ${fattestDog.oz} oz.`);
+    setTimeout(() => console.log("Opening dog profile..."), 2000);
+    setTimeout(() => opener(fattestDog.url), 4000);
   });
